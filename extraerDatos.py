7be581658279d6eb2cd91f2210db6ab719c75209
OscarXD <salazar.oscar69@uabc.edu.mx>
2025-11-30 18:10:56 -0800

    Actualice la funcion de extraccion

--- extraerDatos.py
+++ extraerDatos.py
@@ -2,52 +2,48 @@ import requests
 import pandas as pd
 
 
-def extraerDatos():
-    #Token para usar la api de inegi
+def extraerDatos(inicio, fin, nombreArchivo):
+    """
+    Extrae datos de la API de INEGI en un rango específico
+    """
+    # Token para usar la api de inegi
     token = "b97caaa8-e3c2-40f3-afd6-e5b300217c60"
-    #Codigo que representa las farmacias
+    # Codigo que representa las farmacias
     codigo = "466330"
     palabra = "farmacias"
-    #localidad
-    ciudad = "005"
+    # localidad
     estado = "02"
-    inicio = "1"
-    fin = "1500"
-
 
     url = f"https://www.inegi.org.mx/app/api/denue/v1/consulta/BuscarEntidad/{palabra}/{estado}/{inicio}/{fin}/{token}"
 
-    #Convertir a json lo que extraemos
+    # Convertir a json lo que extraemos
     response = requests.get(url)
     data = response.json()
-    #DATAFRAME DE TODAS LAS FARMACIAS DEL ESTADO
+
+    # df de farmacias del estado
     df = pd.DataFrame(data)
-    df.to_csv("farmaciasEstado.csv",index=False)
-    #df.to_csv("farmaciasEstado2.csv", index=False)
+    df.to_csv(nombreArchivo, index=False)
+    return df
 
-
-    #FILTRAR POR LA COLUMNA UBICACION SOLAMENTE A FARMACIAS DE TIJUANA
-    #dfTijuana = df[df['Ubicacion'] == "TIJUANA, Tijuana, BAJA CALIFORNIA"]
-    #dfTijuana.to_csv("farmaciasTijuana.csv",index=False)
-
-
-    #print(len(df))
-    print("LISTO :)")
 
 def unirDataFrames():
-    #LEER LOS ARCHIVOS
+    #Une los archivos CSV
+    #Lee los CSV
     df1 = pd.read_csv("farmaciasEstado.csv")
     df2 = pd.read_csv("farmaciasEstado2.csv")
 
-    #CONCATENAR LOS DATAFRAMES
-    dfCompleto = pd.concat([df1, df2])
-    dfCompleto.to_csv("farmaciasEstadoCompleto.csv")
-
-    print("LISTO :)")
-
+    # Concatenar los dataframes
+    dfCompleto = pd.concat([df1, df2], ignore_index=True)
+    dfCompleto.to_csv("farmaciasEstadoCompleto.csv", index=False)
 
 
 if __name__ == "__main__":
-    #extraerDatos()
-    unirDataFrames()
+    # Extrae solo los primeros 1500 registros. (La documentación del Api recomiendo no extraer mas de esa cantidad)
+    extraerDatos(inicio=1, fin=1500, nombreArchivo="farmaciasEstado.csv")
 
+    # Extraer siguientes 500 registros
+    extraerDatos(inicio=1501, fin=2000, nombreArchivo="farmaciasEstado2.csv")
+
+    # PASO 3: Unir ambos DataFrames
+    unirDataFrames()
+    print("LISTOOOOO")
